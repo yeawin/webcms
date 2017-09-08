081f1ebd200a906045a085170176be7455dffa32
yeawin <ywliu@xmu.edu.cn>
2017-09-08 10:18:12 +0800

    apache

--- mezza9/mezza9/wsgi.py
+++ mezza9/mezza9/wsgi.py
@@ -10,9 +10,15 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+
 from mezzanine.utils.conf import real_project_name
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "%s.settings" % real_project_name("mezza9"))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",  "%s.settings" % real_project_name("mezza9"))
+
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+
+sys.path.append('C:/Users/Administrator/AppData/Local/Programs/Python/Python36/Lib/site-packages')
 
 application = get_wsgi_application()
